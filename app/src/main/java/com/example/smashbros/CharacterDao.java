a6d420c4a565658df4f5af12433b81120ee1efd4
Spencer Stock <spencerstock@gmail.com>
2019-04-24 22:58:53 -0600

    Initial commit

---
+++ app/src/main/java/com/example/smashbros/CharacterDao.java
@@ -0,0 +1,15 @@
+package com.example.smashbros;
+
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+
+public class CharacterDao {
+    ArrayList<String> urlLinks;
+    ArrayList<Bitmap> icons;
+
+    private ArrayList<String> getUrlLinks() {
+        
+    }
+
+}
---
+++ app/src/main/java/com/example/smashbros/ItemListActivity.java
@@ -0,0 +1,183 @@
+package com.example.smashbros;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.smashbros.dummy.DummyContent;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * An activity representing a list of Items. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link ItemDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class ItemListActivity extends AppCompatActivity {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Document htmlDoc = Jsoup.connect("https://www.ssbwiki.com/Super_Smash_Bros._Ultimate#Characters").get();
+                    String html = htmlDoc.outerHtml();
+                    int startIndex = html.lastIndexOf("<th colspan=\"16\">Veterans (65) </th>");
+                    int endIndex = html.lastIndexOf("<span class=\"mw-headline\" id=\"Bosses\">Bosses</span>");
+                    String subString = html.substring(startIndex, endIndex);
+                    String[] links = subString.split("href");
+                    ArrayList<String> imgLinks = new ArrayList<>();
+                    for (int i = 0; i < links.length; ++i) {
+
+
+                        String regex = "((srcset=\"/images/thumb/)(.*)(png))";
+                        Matcher m = Pattern.compile(regex).matcher(links[i]);
+
+                        if(m.find()) {
+                            String temp = m.group();
+                            int index = temp.lastIndexOf("/images");
+                            temp = temp.substring(index);
+                            imgLinks.add("https://www.ssbwiki.com" + temp);
+                        }
+                        String temp = "Hello";
+                    }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        if (findViewById(R.id.item_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+
+        View recyclerView = findViewById(R.id.item_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, mTwoPane));
+    }
+
+    public static class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final ItemListActivity mParentActivity;
+        private final List<DummyContent.DummyItem> mValues;
+        private final boolean mTwoPane;
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DummyContent.DummyItem item = (DummyContent.DummyItem) view.getTag();
+                if (mTwoPane) {
+                    Bundle arguments = new Bundle();
+                    arguments.putString(ItemDetailFragment.ARG_ITEM_ID, item.id);
+                    ItemDetailFragment fragment = new ItemDetailFragment();
+                    fragment.setArguments(arguments);
+                    mParentActivity.getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.item_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, ItemDetailActivity.class);
+                    intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, item.id);
+
+                    context.startActivity(intent);
+                }
+            }
+        };
+
+        SimpleItemRecyclerViewAdapter(ItemListActivity parent,
+                                      List<DummyContent.DummyItem> items,
+                                      boolean twoPane) {
+            mValues = items;
+            mParentActivity = parent;
+            mTwoPane = twoPane;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mIdView.setText(mValues.get(position).id);
+            holder.mContentView.setText(mValues.get(position).content);
+
+            holder.itemView.setTag(mValues.get(position));
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            final TextView mIdView;
+            final TextView mContentView;
+
+            ViewHolder(View view) {
+                super(view);
+                mIdView = (TextView) view.findViewById(R.id.id_text);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+        }
+    }
+}
